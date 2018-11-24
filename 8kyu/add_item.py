41617a3f687b792d5c2b57363024939634387df6
Uthaeus <romanlavery@gmail.com>
2018-11-24 15:17:11 -0700

    Add the value "codewars" to the websites array

---
+++ 8kyu/add_item.py
@@ -0,0 +1,10 @@
+# Add the value "codewars" to the websites array.
+# After your code executes the websites array should == ["codewars"]
+
+websites = []
+
+def add_item(item):
+    websites.append(item)
+
+add_item('codewars')
+print(websites)
