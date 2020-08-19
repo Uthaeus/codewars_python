1d2d8c5c061229a40ba4eb15fc0ad8d6d922851a
Uthaeus <romanlavery@gmail.com>
2020-08-18 19:10:23 -0600

    Make a function that will return a greeting statement that uses an input

---
+++ 8kyu/returning_strings.py
@@ -0,0 +1,8 @@
+# Make a function that will return a greeting statement that uses an input
+
+
+def greet(name):
+    return "Hello, " + name + " how are you doing today?"
+
+
+print(greet('Ryan')) #, "Hello, Ryan how are you doing today?")
