dc7f78f385acadea339af402dc3d5c3113fd54d9
Uthaeus <romanlavery@gmail.com>
2020-08-19 16:25:36 -0600

    you will be given a number in form of a string and an integer k and your task is to insert k commas into the string and determine which of the partitions is the largest

---
+++ 7kyu/string_division.py
@@ -0,0 +1,9 @@
+# In this Kata, you will be given a number in form of a string and an integer k and your task is to insert k commas into the string and determine which of the partitions is the largest.
+
+
+def solve(st,k):
+    pass
+
+
+
+print(solve('1234',1)) #,234)
