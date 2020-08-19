cae4095f8158e4b3ee25d79a6a52b352a75465a9
Uthaeus <romanlavery@gmail.com>
2020-08-18 19:55:38 -0600

    You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value

---
+++ 8kyu/hydrated.py
@@ -0,0 +1,10 @@
+import math 
+# You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+
+def litres(time):
+    total = int(time * 0.5)
+
+    return total
+
+print(litres(2)) #, 1)
