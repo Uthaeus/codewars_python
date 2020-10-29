b9db6c32f30768c3880b0f4be9c4332698b829d5
Uthaeus <romanlavery@gmail.com>
2020-10-29 09:41:33 -0600

    Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point

---
+++ 7kyu/greet_me.py
@@ -0,0 +1,8 @@
+# Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+
+def greet(name): 
+    return 'Hello ' + name.capitalize() + '!'
+
+
+print(greet('molly')) #, "Hello Molly!"
