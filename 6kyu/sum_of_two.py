feb41ee2c1e51467f50ec1ea58deb76c71bcb5d0
Uthaeus <romanlavery@gmail.com>
2020-10-30 08:34:30 -0600

    correcting bug

--- 6kyu/sum_of_two.py
+++ 6kyu/sum_of_two.py
@@ -2,18 +2,18 @@
 
 
 
-  while (y != 0):
+  # while (y != 0):
        
-          # carry now contains common
-          # set bits of x and y
-          carry = x & y
+  #         # carry now contains common
+  #         # set bits of x and y
+  #         carry = x & y
    
-          # Sum of bits of x and y where at
-          # least one of the bits is not set
-          x = x ^ y
+  #         # Sum of bits of x and y where at
+  #         # least one of the bits is not set
+  #         x = x ^ y
    
-          # Carry is shifted by one so that   
-          # adding it to x gives the required sum
-          y = carry << 1
+  #         # Carry is shifted by one so that   
+  #         # adding it to x gives the required sum
+  #         y = carry << 1
        
-      return x
+  #     return x
